5012c098fce02d6d827a5d0564cfe70e941c024c
D M <ryki.admirala@gmail.com>
2016-11-30 18:04:44 +0200

    some kind of tabs is ready

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function tabs() {
+  var $tabLinks = $('.tabs-block').find('.tabs-head a');
+  var $tabBlocks = $('.tabs-block').find('.tabs-item');
+  
+
+  $tabLinks.each(function(i) {
+    $(this).attr('tab-id', i);
+  });
+  $tabBlocks.each(function(i) {
+    $(this).attr('tab-id', i);
+  });
+  
+  
+  $tabLinks.on('click', function(e) {
+    e.preventDefault();
+    var index = $(this).attr('tab-id');
+      
+    $tabLinks.removeClass('active-link');
+    $(this).addClass('active-link');
+    
+    $tabBlocks.each(function() {
+      if ($(this).attr('tab-id') == index) {
+        $(this).addClass('active').siblings('.tabs-item').removeClass('active');
+      }
+    });    
+  });
+    
+    
+});
