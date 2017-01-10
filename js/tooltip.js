8471bbd0153d1415581e68209f871fb7be9c763a
D M <ryki.admirala@gmail.com>
2017-01-10 18:03:04 +0200

    STILL TRYING CMON MAN

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,6 +1,6 @@
 $(function tooltip() {
   var showall;
-  var $tooltipItem = $('.tooltip-block').find('[tooltip]'); // Elements for which we are making tooltip
+  var $tooltipItem = $('.has-tooltip'); // Elements for which we are making tooltip
 
   $tooltipItem.on('mouseenter focusin', showTooltip);
   $tooltipItem.on('mouseleave focusout', hideTooltip);
@@ -10,15 +10,21 @@ $(function tooltip() {
   function showTooltip(showall) {
     var $tooltip = $(this).next('.tooltip');
 
-
     if ( !$tooltip.length || (!$tooltip.length && showall) ) {
       // If there is no tooltip OR function is called by 'Show All Tooltips' button
-     console.log('show');
+      var ttOffset = $(this).offset();
+      var ttHeight = $(this).height();
+      var ttWidth = $(this).width();
+      var ttText = $(this).attr('title');
+      
+      $(this).attr('title', '');
 
-      var tooltipText = $(this).attr('tooltip');
-
-      $('<span class="tooltip">' + tooltipText + '</span>')
+      $('<span class="tooltip">' + ttText + '</span>')
                   .insertAfter($(this))
+                  .css({
+                    'top' : ttOffset.top - ttHeight/2,
+                    'left' : ttOffset.left + ttWidth + 20
+                  })
                   .fadeIn(250);
     }
   }
@@ -27,13 +33,14 @@ $(function tooltip() {
     var $tooltip = $(this).next('.tooltip');
     if ( !( $(this).is(':focus') || $(this).is(':hover') ) ) {
       // Making tooltip NOT to disappear on focused or hovered tooltip'ed item
-     console.log('hide');
+      $(this).attr('title', $tooltip.text());
 
       $tooltip
         .stop(true)
         .fadeOut(150, function() {
           $(this).remove();
         });
+        
 
     }
   }
